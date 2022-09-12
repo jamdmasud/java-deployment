f06bd9f96ec207d852ef7c1cf4869b46d8a2521b
Md. Masud <jamdmasud@gmail.com>
2022-09-12 18:17:20 +0600

    appspec.yml is updated

--- install.sh
+++
@@ -1,4 +0,0 @@
-#sudo service tomcat stop
-#aws s3 cp s3://groupies/java-codebuild/hello-world-0.0.1-SNAPSHOT.war /tmp
-#sudo mv /tmp/hello-world-0.0.1-SNAPSHOT.war /home/ubuntu/HelloWorld/hello-world-0.0.1-SNAPSHOT.war
-sudo service tomcat restart
